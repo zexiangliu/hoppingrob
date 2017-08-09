0138e97b979f57fef4d4c73fd044daab8d49daa1
Your Name <you@email.com>
2017-08-09 22:45:19 +0800

    change the way of initializing the parfor in noninear_model1

--- demo/nonlinear_model1/initial.m
+++ demo/nonlinear_model1/initial.m
@@ -4,9 +4,6 @@ addpath(genpath('../../lib/abstr-ref/'));
 addpath('../../lib/GridGener/');
 addpath('../../lib/nonlinear_approx/');
 addpath('../../lib/console/');
+addpath('../../lib/Parallel/');
 
-delete(gcp('nocreate'));
-if(~exist('thePool','var'))
-    NP = 4;
-    thePool = parpool('local',4);
-end
+specify_num_workers(4);
