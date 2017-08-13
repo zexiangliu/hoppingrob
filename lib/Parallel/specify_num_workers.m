e3a045c06f0e93d06c1d66be1036bbb2262eaa83
Your Name <you@email.com>
2017-08-13 11:42:04 -0400

    try to fix parallel problem in flux

--- lib/Parallel/specify_num_workers.m
+++ lib/Parallel/specify_num_workers.m
@@ -1,4 +1,4 @@
-function y = specify_num_workers(w)
+function myPool = specify_num_workers(w)
 % input: w  num of workers
 % Note: If no input, the program will detect the num of processors defined
 % by PBS file as w or use default number 4.
@@ -9,9 +9,15 @@ function y = specify_num_workers(w)
         w = str2double(getenv('PBS_NP'));
     end
   end
-  y = ones(1,100);
-  % w specifies maximum number of workers
-  parfor (i = 1:100,w)
-    y(i) = i;
+  try
+      myPool = parpool('local', w);
+  catch
+      delete(gcp('nocreate'));
+      myPool = parpool('local', w);
   end
+%   y = ones(1,100);
+%   % w specifies maximum number of workers
+%   parfor (i = 1:100,w)
+%     y(i) = i;
+%   end
 end
