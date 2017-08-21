9a8640ab82730a8d6cfa8755b083cecabe3dbb17
Your Name <you@email.com>
2017-08-21 00:41:03 -0400

    modify tag.m in nonlinear_model1

--- demo/nonlinear_model1/tag.m
+++ demo/nonlinear_model1/tag.m
@@ -5,7 +5,7 @@ function info = tag()
     % Define how to execute the example in a fast way, by using abstr. and
     % cont saved previously.
     info.fast = 'initial;abstraction;';
-    info.flux = 'initial;test_flux;';
+    info.flux = 'initial;test_flux;test_flux2;';
     if(exist('test.mat','file')==2&&exist('rt_ts.mat','file')==2)
         info.fast_able = 1;
     else 
