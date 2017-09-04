5b86c733f403cd38ab3e64afbc20ef7a1a3ffece
Your Name <you@email.com>
2017-09-03 22:14:57 -0400

    modify the tag of PatchCont_case1

--- demo/PatchCont_case1/tag.m
+++ demo/PatchCont_case1/tag.m
@@ -1,12 +1,12 @@
 function info = tag()
-    info.nametag = 'push_rec_case1';
+    info.nametag = 'patchcont_case1';
     info.description = 'Test PatchCont lib using the code in push_recovery/case1 ';
     % Define how to execute the example
-    info.run = 'initial;abstraction;simulation;animation';
+    info.run = 'initial;abstraction;test;test2';
     % Define how to execute the example in a fast way, by using abstr. and
     % cont saved previously.
-    info.fast = 'initial;simulation;animation;';
-    if(exist('ts.mat','file')==2)
+    info.fast = 'initial;test;test2;';
+    if(exist('ts_ref.mat','file')==2)
         info.fast_able = 1;
     else 
         info.fast_able = 0;
